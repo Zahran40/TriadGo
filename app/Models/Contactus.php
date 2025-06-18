aa1e90112e3e63377fd24ddc4164c76504ad53e2
Andre <andrefarizisebayang@gmail.com>
2025-06-18 21:26:07 +0700

    perbaikan sedikit error

--- app/Models/Contactus.php
+++ app/Models/Contactus.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $message
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 class Contactus extends Model
 {
     //

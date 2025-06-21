ccf14d7f27e00c6bddf67679da6fd71caacab2b0
andrefarizi <andrefarizisebayang@gmail.com>
2025-06-21 12:22:56 +0700

    menghapus file tidak berguna

--- resources/views/filament/widgets/empty-space-widget.blade.php
+++
@@ -1,7 +0,0 @@
-<x-filament-widgets::widget>
-    <x-filament::section>
-        <div class="p-4">
-            <!-- Ruang kosong untuk keseimbangan layout -->
-        </div>
-    </x-filament::section>
-</x-filament-widgets::widget>
--- resources/views/filament/widgets/placeholder-widget.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-widgets::widget>
-    <x-filament::section>
-        <div class="text-center py-4">
-            <p class="text-gray-500 dark:text-gray-400">
-                Placeholder content
-            </p>
-        </div>
-    </x-filament::section>
-</x-filament-widgets::widget>

486a1e996b66e4a095665b87eabcdd18ec060287
andrefarizi <andrefarizisebayang@gmail.com>
2025-06-21 12:24:47 +0700

    mengembalikan file tidak berguna yang sebelumnya dihapus (menyebabkan error)

---
+++ resources/views/filament/widgets/placeholder-widget.blade.php
@@ -0,0 +1,9 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <div class="text-center py-4">
+            <p class="text-gray-500 dark:text-gray-400">
+                Placeholder content
+            </p>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
